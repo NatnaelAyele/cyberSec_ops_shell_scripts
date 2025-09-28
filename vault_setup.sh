68481d3ac6b56b95962a0f3a39dea166d3a3277a
NatnaelAyele <n.eticha@alustudent.com>
2025-09-28 03:15:59 -0700

    created a shell script that initializes the vault we will continue to work on

---
+++ vault_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+directory=secure_vault
+mkdir -p ~/secure_vault
+cd secure_vault
+touch keys.txt secrets.txt logs.txt
+for file in keys.txt secrets.txt logs.txt; do
+	echo "Welcome $USER" > $file
+done
+
+if [ -d "$directory" ]; then
+	all_files_exist=true
+	for file in *.txt; do
+		if [ ! -f "$file" ]; then
+			all_files_exist=false
+			break
+		fi
+	done
+	if [ "$all_files_exist" = true ]; then
+		echo "All files are created successsfuly"
+		ls -l
+	else
+		echo "Not all files are created"
+	fi
+else
+	echo "Directory not found"
+fi
