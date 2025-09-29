bbcaad2bca506b844e2d0a400074da8db90d1fee
NatnaelAyele <n.eticha@alustudent.com>
2025-09-29 01:57:49 -0700

    script to generate reports of all the file in the vault

---
+++ vault_monitor.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+directory=~/secure_vault
+
+> report.txt
+
+if [ -d "$directory" ]; then
+    for file in keys.txt secrets.txt logs.txt; do
+        file_path="$directory/$file"
+        if [ -f "$file_path" ]; then
+            size=$(stat -c%s "$file_path")
+            mod_date=$(stat -c%y "$file_path")
+            permissions=$(stat -c%A "$file_path")
+            echo "File name: $file" >> report.txt
+            echo "Size: $size bytes" >> report.txt
+            echo "Modified: $mod_date" >> report.txt
+            echo "Permissions: $permissions" >> report.txt
+        fi
+    done
+else
+    echo "secure vault not found"
+fi
+
